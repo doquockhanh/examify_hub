2a47d2cf32e04ad19cf24f2e470c7a4b0549d64c
doquockhanh <khanhquocdo.qt@gmail.com>
2023-10-12 18:29:39 +0700

    add routing between home n exam page

--- pages/examify.tsx
+++ pages/examify.tsx
@@ -104,7 +104,7 @@ function ExamifyPage() {
     return (
         <div className="container mx-auto p-4 px-[25%] bg-slate-300 min-h-screen">
             <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex py-6 pb-12">
-                <Link href="/examify">
+                <Link href="../">
                     <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
                         Go to&nbsp;
                         <code className="font-mono font-bold">Examify</code>
